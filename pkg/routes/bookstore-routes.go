33765be5b919b5401f8f3c31a291833d5b7227d4
udodinho <udodinho200@gmail.com>
2023-09-09 06:59:39 -0700

    Remove comments

--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -1,11 +1,7 @@
 package routes
 
 import (
-	// "fmt"
-	// "log"
-
 	"github.com/gofiber/fiber/v2"
-	// "github.com/joho/godotenv"
 	"github.com/udodinho/bookstore/pkg/controllers"
 	"gorm.io/gorm"
 )
@@ -14,12 +10,12 @@ type Repository struct {
 	DB *gorm.DB
 }
 
-func(r *Repository) SetupRoutes(app *fiber.App){
+func (r *Repository) SetupRoutes(app *fiber.App) {
 	api := app.Group("/api/v1/books")
 	api.Get("/", controllers.GetAllBooks)
 	api.Post("/", controllers.CreateBook)
 	api.Get("/:id", controllers.GetBook)
-	api.Put ("/:id", controllers.UpdateBook)
+	api.Put("/:id", controllers.UpdateBook)
 	api.Delete("/:id", controllers.DeleteBook)
 
 }
