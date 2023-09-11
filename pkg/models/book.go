6bb8cd82d834a7db4e5ac2607bd750ad2956b73f
udodinho <udodinho200@gmail.com>
2023-09-11 14:27:33 -0700

    Create book models

---
+++ pkg/models/book.go
@@ -0,0 +1,60 @@
+package models
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/udodinho/bookstore/pkg/config"
+	"gorm.io/gorm"
+)
+
+type Books struct {
+	ID uint			`gorm:"primarykey;autoIncrement" json:"id"`
+	Author *string	`json:"author"`
+	Title *string	`json:"title"`
+	Publisher *string	`json:"publisher"`
+}
+
+var DB *gorm.DB
+
+func MigrateBooks(db *gorm.DB) error {
+	err := db.AutoMigrate(&Books{}) 
+
+	return err
+}
+
+
+func init() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	configFile := &config.Config{ 
+		Host: os.Getenv("DB_HOST"),
+		Port: os.Getenv("DB_PORT"),
+		Password: os.Getenv("DB_PASS"),
+		User: os.Getenv("DB_USER"),
+		DBName: os.Getenv("DB_NAME"),
+		SSLMode: os.Getenv("DB_SSLMODE"),
+
+	}
+	db, err := config.Connect(configFile)
+
+	if err != nil {
+		log.Fatal("Could not connect to db", err)
+	}
+
+	err = MigrateBooks(db)
+
+	if err != nil {
+		log.Fatal("Could not migrate db", err)
+	}
+
+	DB = db
+	fmt.Println("Database connected successfully")
+	
+}
